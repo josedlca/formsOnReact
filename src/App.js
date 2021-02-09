889807a0df618fc20973b259cc00982f856519c4
josedlca <jose.dlcruz.ayala@gmail.com>
2021-02-09 10:08:40 -0500

    forms part one

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      Hello
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super()
+    this.state = {
+      firstName: "",
+      lastName: ""
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(event){
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  render(){
+    return(
+      <form>
+        <input type="text" name="firstName" placeholder="First Name" onChange={this.handleChange}/>
+        <br/>
+        <input type="text" name="lastName" placeholder="Last Name" onChange={this.handleChange}/>
+        <h1>{this.state.firstName} {this.state.lastName}</h1>
+      </form>
+    )
+  }
+
 }
 
 export default App;
